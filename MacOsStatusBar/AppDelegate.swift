465c99ab94622b991fb6a173f51f6dc089839208
honda <hondayuki0606@yahoo.co.jp>
2020-10-08 17:23:27 +0900

    Initial Commit

---
+++ MacOsStatusBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOsStatusBar
+//
+//  Created by 本田尚行 on 2020/10/08.
+//  Copyright © 2020 本田尚行. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOsStatusBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOsStatusBar
+//
+//  Created by 本田尚行 on 2020/10/08.
+//  Copyright © 2020 本田尚行. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
