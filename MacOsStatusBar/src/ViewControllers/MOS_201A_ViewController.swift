bb647a83904df55b118ad1811b4228708140d28a
honda <hondayuki0606@yahoo.co.jp>
2020-10-25 19:56:18 +0900

    ViewController追加

---
+++ MacOsStatusBar/src/ViewControllers/MOS_201A_ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MOS_201A_ViewController.swift
+//  MacOsStatusBar
+//
+//  Created by 本田尚行 on 2020/10/25.
+//  Copyright © 2020 本田尚行. All rights reserved.
+//
+
+import Foundation
---
+++ MacOsStatusBar/src/ViewControllers/MOS_202A_ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  APIClient.swift
+//  CafeSearch
+//
+//  Created by 本田尚行 on 2020/09/17.
+//  Copyright © 2020 本田尚行. All rights reserved.
+//
+
+import Cocoa
+
+class MOS_202A_ViewController {
+    
+    func MOS_202A_FontDownload() {
+        
+        let alert = NSAlert()
+        // 表示メッセージとインフォメーション
+        alert.messageText = "MOS202Aフォントダウンロード"
+        alert.informativeText = "MOS202Aのフォントをダウンロードします。よろしいですか？"
+        // メッセージのスタイル
+        alert.alertStyle = .informational
+        // ボタン追加
+        alert.addButton(withTitle: "キャンセル")
+        alert.addButton(withTitle: "OK")
+        // ボタンに対してショートカットキーの設定する場合
+        let buttons = alert.buttons
+        buttons[0].keyEquivalent = "\u{1b}"    // esc key
+        // buttons[0].tag = 1234               // runModalの戻り値を変更する場合
+        buttons[1].keyEquivalent = "\r"        // enter key
+        
+        // 表示
+        let ret = alert.runModal()
+
+        switch ret {
+        case .alertFirstButtonReturn:
+            let vm = MOS_202A_ViewModel()
+            vm.multipartPost()
+
+        case .alertSecondButtonReturn:
+            print("キャンセル")
+        default:
+            print("キャンセル:\(ret)")
+        }
+    }
+}
